8a2bb8df0b677bb70ad5a49f3ecff7ce1192c9c5
Leonor Colbert <leonoracolbert@gmail.com>
2020-03-31 19:35:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,6 +44,18 @@ class Student
     end
   end
   
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+    
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
  def self.first_X_students_in_grade_10(limit)
     sql = <<-SQL
       SELECT *
